56cc4aae542308e92eec720d3074db3d6ec9e3f3
shubh <bittu7shubham@gmail.com>
2024-07-08 13:27:38 +0530

    third commit

--- app/components/SearchHistoryList.jsx
+++ app/components/SearchHistoryList.jsx
@@ -4,16 +4,13 @@ import PropTypes from "prop-types";
 
 const SearchHistoryList = ({ searchHistory, onSelect }) => {
   return (
-    <div
-      //   className="absolute bg-white shadow-md rounded-lg top-full mt-2 w-full max-h-60 overflow-y-auto z-20 border border-gray-300"
-      style={{ flex: "1 1 0 !important" }}
-    >
+    <div className="search-history-list">
       <ul className="list-none p-2">
         {searchHistory.map((item, index) => (
           <li
             key={index}
             className="p-2 hover:bg-gray-200 cursor-pointer"
-            onMouseDown={() => onSelect(item)} // Use onMouseDown to avoid blur issue
+            onMouseDown={() => onSelect(item)}
           >
             {item}
           </li>
@@ -23,7 +20,6 @@ const SearchHistoryList = ({ searchHistory, onSelect }) => {
   );
 };
 
-// Define prop types for validation
 SearchHistoryList.propTypes = {
   searchHistory: PropTypes.arrayOf(PropTypes.string).isRequired,
   onSelect: PropTypes.func.isRequired,
